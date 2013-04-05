11fbea7f9b7b2fa331ec02e0d6972c9119cddfbb
craniuzin <craniuzin@gmail.com>
2013-04-05 19:41:40 -0300

    Using Rumblefish API for search

--- ajax/moodslist_table.php
+++ ajax/moodslist_table.php
@@ -12,20 +12,24 @@ if (isset($_REQUEST['y'])){
 $workingDir = dirname(__FILE__);
 
 $terms = array(
-    "genre01" => array('xs' => 0, 'xe' => 100, 'ys' => 0, 'ye' => 100),
-    "genre02" => array('xs' => 0, 'xe' => 100, 'ys' => -100, 'ye' => 0),
-    "genre03" => array('xs' => -100, 'xe' => 0, 'ys' => 0, 'ye' => 100),
-    "genre04" => array('xs' => -100, 'xe' => 0, 'ys' => -100, 'ye' => 0),
+    "Rock" => array('xs' => 0, 'xe' => 100, 'ys' => 0, 'ye' => 100),
+    "Blues" => array('xs' => 0, 'xe' => 100, 'ys' => -100, 'ye' => 0),
+    "Jazz" => array('xs' => -100, 'xe' => 0, 'ys' => 0, 'ye' => 100),
+    "Classical" => array('xs' => -100, 'xe' => 0, 'ys' => -100, 'ye' => 0),
 );
 
 
 $match = array();
+$genre = '';
 
 foreach($terms as $term => $coords){
     if($x >= $coords['xs'] && $x <= $coords['xe'] && $y >= $coords['ys'] && $y <= $coords['ye']){
        $match[] = $term;
+       $genre .= "&genre[]={$term}";
     }
 }
+
+$contents = json_decode(file_get_contents('https://sandbox.rumblefish.com/v2/search?&token=SS49OqFQnbpNkmHY1Q2kZpCjmvu2QNb3'.$genre), true);
 
 ?>
 <div id="matched-terms">
@@ -35,4 +39,11 @@ foreach($terms as $term => $coords){
         <li><?php echo $term; ?></li>
         <?php endforeach; ?>
     </ol>
+    <hr />
+    <h2>Num Medias found: <?php echo $contents['total_media']; ?></h2>
+    <ul>
+       <?php foreach($contents['media'] as $media): ?>
+       <li><?php echo $media['artists'][0]['name'] ?> - <?php echo $media['title']; ?></li>
+       <?php endforeach; ?>
+    </ul>
 </div>
