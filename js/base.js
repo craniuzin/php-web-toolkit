bf7fe4576175165c9ea60147db60d59899addfb2
craniuzin <craniuzin@gmail.com>
2013-04-09 10:59:14 -0300

    Color blending

--- js/base.js
+++ js/base.js
@@ -5,10 +5,23 @@ $(document).ready(function(){
       max: 100,
       min: -100,
       value: 0,
-      slide: function(){
-        var value = $(this).slider('value');
-        $(this).parent().find('em').text(value);
-      }
+      slide: sliding
     });
 });
+
+function sliding(){
+    var value = $(this).slider('value');
+    $(this).parent().find('em').text(value);
+
+    // Colors
+    var positive = $(this).css('background-color');
+    var negative = $(this).find('.ui-slider-range').css('background-color');
+    
+    positive = positive.replace('rgb(', '').replace(')', '').split(', ');
+    negative = negative.replace('rgb(', '').replace(')', '').split(', ');
+
+    var direction = (parseInt(value) > 0) ? "positive" : "negative";
+
+    console.log(positive, negative, direction);
+}
 
